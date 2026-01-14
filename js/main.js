b3b5649d021e7bd58169c808a8a7543ef5c5e135
Dmlnk <milenkyi.777@gmail.com>
2026-01-14 15:05:56 +0200

    up to section benefits

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// =============== Mobile menu buttons ===============================
+const openMobileMenu = document.querySelector('.mobile-menu-open')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+function openMenuHandler () {
+    console.log("hello world")
+    mobileMenu.classList.add('is-open')
+
+}
+openMobileMenu.addEventListener('click', openMenuHandler)
+
+const closeMobileMenu = document.querySelector('.mobile-menu-close')
+
+function closeMenuHandler() {
+    console.log("bye bye world")
+    mobileMenu.classList.remove('is-open')
+
+}
+closeMobileMenu.addEventListener('click', closeMenuHandler)
+
+// =====================About me accordion=====================================
+
+new Accordion(".accordion-container");
+  
+const rotateBtn = document.querySelectorAll(' .ac-trigger-svg');
+
+function clickHandler() {
+    this.classList.toggle('ac-is-open');
+
+}
+for (let btn of rotateBtn) {
+    btn.addEventListener('click', clickHandler);
+}
+
+//  =======================About me swiper==============================
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 400,
+    loopedSlides: 6,
+    slidesPerView: 'auto',
+        
+    mouseweel: {
+        invert: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+   
+});
+
+
+
+
+
+
+
